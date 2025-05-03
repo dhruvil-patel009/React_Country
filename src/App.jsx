7518486c912f495382b7fb63eb679506951224fd
Dhruvil Patel <85049831+dhruvil-patel009@users.noreply.github.com>
2025-05-03 23:32:13 +0530

    Created a Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Country } from './pages/Country'
+import { Contact } from './pages/Contact'
+import { AppLayout } from './components/Layout/AppLayout'
+import { ErrorPage } from './pages/ErrorPage'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [{
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: 'about',
+      element: <About />
+    },
+    {
+      path: 'country',
+      element: <Country />
+    },
+    {
+      path: 'contact',
+      element: <Contact />
+    },]
+  }
+
+])
 const App = () => {
-  return <h1>Hello Country </h1>
-}
 
+  return <RouterProvider router={router}></RouterProvider>;
+
+}
 export default App
