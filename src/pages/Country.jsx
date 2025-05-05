5dc91f42d79c5ab61fa021fa177e856fc91a3345
Dhruvil Patel <85049831+dhruvil-patel009@users.noreply.github.com>
2025-05-05 23:27:27 +0530

    Country Page Card Create

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,21 +1,31 @@
-import { useTransition } from "react"
+import { useState, useTransition } from "react"
 import { useEffect } from "react"
 import { getCountryData } from "../api/postApi"
 import { Loader } from "../components/UI/Loader"
+import { CountryCard } from "../components/Layout/CoutryCard"
 
 export const Country = () => {
     const [isPending, startTransition] = useTransition()
-    // const [countries,setCountries]= useState([]);
+    const [countries, setCountries] = useState([]);
 
-    useEffect(()=>{
-        startTransition(async()=>{
+    useEffect(() => {
+        startTransition(async () => {
             const res = await getCountryData();
-            // setCountries(res.data)
+            setCountries(res.data)
+            console.log(res.data)
         })
-    },[])
-    
-    if(isPending)return <Loader/>
+    }, [])
+
+    if (isPending) return <Loader />
     return (
-        <h1></h1>
+        <section className="country-section">
+            <ul className="grid grid-four-cols">
+                {
+                    countries.map((curlCountry, index) => {
+                        return <CountryCard country={curlCountry} key={index} />
+                    })
+                }
+            </ul>
+        </section>
     )
 }
